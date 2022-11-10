b011c5d2fef38bddbbabf5fbdf7da7ed61ec1b88
Alfred Encinar <220027@fppiramide.com>
2022-11-10 13:16:15 +0100

    h

--- src/Gato.java
+++ src/Gato.java
@@ -1,5 +1,28 @@
 public class Gato {
     public static void main(String[] args) {
-        System.out.println("Hola gatos del mundo");
+        System.out.println("Hola gatos del mundo");}
+
+    private String nombre;
+
+    private String raza;
+
+    private String color;
+
+    private String numChip;
+
+    private int edad;
+
+    Gato(String no , String ra , String co , String nc , int ed){
+
+        this.nombre = no;
+        this.raza = ra;
+        this.color = co;
+        this.numChip = nc;
+        this.edad = ed;
+
+    }
+
+    public String getNombre() {
+        return nombre;
     }
 }
