beb657d03ef3662dea23d202e39db7a82357fcde
Alfred Encinar <220027@fppiramide.com>
2022-11-14 10:02:49 +0100

    h

--- src/Gato.java
+++ src/Gato.java
@@ -8,17 +8,32 @@ public class Gato {
 
     private String color;
 
+    private String pelo;
+
     private String numChip;
 
     private int edad;
 
-    Gato(String no , String ra , String co , String nc , int ed){
+    Gato(String no , String ra , String co , String p ,String nc , int ed){
 
         this.nombre = no;
         this.raza = ra;
         this.color = co;
+        this.pelo = p;
         this.numChip = nc;
         this.edad = ed;
+
+    }
+
+    public String toString(){
+
+        String salida;
+
+        salida = String.format("nombre : %s\nraza : %s\ncolor : %s\npelo : %s\nnúmero de chip : %s\nedad : %s años " ,nombre,raza,color,pelo,numChip,edad);
+/*        salida = "Este es el libro titulado " + this.titulo;
+        salida += ", el autor es " + this.autor;*/
+
+        return salida;
 
     }
 
---
+++ src/usoGato.java
@@ -0,0 +1,11 @@
+public class usoGato {
+    public static void main(String[] args) {
+
+        Gato miGato = new Gato("Andrés", "Maicon" , "Negro" , "peludo" , "16ak8J" , 5 );
+
+        System.out.println(miGato);
+
+    }
+
+
+}
